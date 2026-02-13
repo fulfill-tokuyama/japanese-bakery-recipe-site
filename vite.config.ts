7044499d0b1e36b325b90b38146a6eaff46f8cbf
fulfill-tokuyama <t.tokuyama@fulfill-net.com>
2026-02-13 21:33:24 +0900

    Set base path for deployment in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/japanese-bakery-recipe-site/',
       server: {
         port: 3000,
         host: '0.0.0.0',
